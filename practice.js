3f0b49eaa7b0063f290fb4ff2a93f30137d59c54
Sanika <sanikapatil1108@gmail.com>
2026-07-13 12:56:31 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,12 @@
+let color="red";
+
+//Traffic Light System
+if(color=red){
+    console.log("Stop");
+}
+if(color==="yellow"){
+    console.log("Be ready!");
+}
+if (color==="green"){
+    console.log("Go!");
+}
